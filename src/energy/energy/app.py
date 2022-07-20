433f2e98717bbc250adf60d37b03f5a3f94c35bc
Sruinard <41334908+Sruinard@users.noreply.github.com>
2022-07-20 06:38:46 +0000

    update app.py

--- src/energy/energy/app.py
+++ src/energy/energy/app.py
@@ -1,6 +1,8 @@
 import strawberry
+import uvicorn
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
+
 from energy import queries
 
 schema = strawberry.Schema(query=queries.Query)
@@ -9,3 +11,6 @@ graphql_app = GraphQL(schema)
 app = FastAPI()
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
+
+if __name__ == "__main__":
+    uvicorn.run(app, )
